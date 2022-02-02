b52de435f20d05edaaf60d5caeae8e51168cf280
DariaKutuzova <kutuzovad94@gmail.com>
2022-02-03 00:28:44 +0300

    refactor: Изменить домен в auth

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co'
+export const BASE_URL = 'https://kutuzova.mesto.students.nomoredomains.work'
 
 function handleResponse(res) {
     if (res.ok) {
